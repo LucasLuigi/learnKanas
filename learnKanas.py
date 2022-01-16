9e87f51b41267aa382823c1c8392a32d9d809546
LucasLuigi <lukilucas@hotmail.fr>
2022-01-16 03:35:43 +0100

    Added Katakanas (missing img for the moment) Added Dakuon, Handakuon and combos for both alphabet (missing img for the moment) Refactored the code (generic functions for hiraganas, katakanas and every subsets of kanas, deleted useless constants [SYMBOL]NUMBER, renamed some variables, converted some prints to f-string)

--- learnKanas.py
+++ learnKanas.py
@@ -4,8 +4,12 @@ import sys
 import random
 from PIL import Image
 
-KANASNUMBER = 46
-kanas = {
+ALPHABETS = ['Hiragana', 'Katakana', 'Kanji']
+
+# The order must match with the input of selectKanasSubset
+KANASSUBSETS = ["simple kanas", "Dakuon", "Handakuon", "combo kanas"]
+
+SIMPLEKANAS = {
     "": ["a", "i", "u", "e", "o"],
     "K": ["ka", "ki", "ku", "ke", "ko"],
     "S": ["sa", "shi", "su", "se", "so"],
@@ -19,66 +23,154 @@ kanas = {
     "n": ["n"]
 }
 
+DAKUON = {
+    "G": ["ga", "gi", "gu", "ge", "go"],
+    "Z": ["za", "z_ji", "zu", "ze", "zo"],
+    "D": ["da", "d_ji", "ju", "de", "do"],
+    "B": ["ba", "bi", "bu", "be", "bo"]
+}
 
-def randomRomajiToKana():
-    global KANASNUMBER
-    global kanasList
+HANDAKUON = {
+    "P": ["pa", "pi", "pu", "pe", "po"]
+}
+
+COMBOSKANAS = {
+    "K": ["kya", "kyu", "kyo"],
+    "G": ["gya", "gyu", "gyo"],
+    "S": ["sha", "shu", "sho"],
+    "J": ["ja", "ju", "jo"],
+    "C": ["cha", "chu", "cho"],
+    "N": ["nya", "nyu", "nyo"],
+    "H": ["hya", "hyu", "hyo"],
+    "B": ["bya", "byu", "byo"],
+    "P": ["pya", "pyu", "pyo"],
+    "M": ["mya", "myu", "myo"],
+    "R": ["rya", "ryu", "ryo"]
+}
+
+
+def initListsFromKanasDicts():
+    global simpleKanasList
+    global dakuonList
+    global handakuonList
+    global combosKanasList
+    global everyKanasList
+
+    simpleKanasList = []
+    for line in SIMPLEKANAS:
+        for kana in SIMPLEKANAS[line]:
+            simpleKanasList.append(kana)
+
+    dakuonList = []
+    for line in DAKUON:
+        for kana in DAKUON[line]:
+            dakuonList.append(kana)
+
+    handakuonList = []
+    for line in HANDAKUON:
+        for kana in HANDAKUON[line]:
+            handakuonList.append(kana)
+
+    combosKanasList = []
+    for line in COMBOSKANAS:
+        for kana in COMBOSKANAS[line]:
+            combosKanasList.append(kana)
+
+    everyKanasList = simpleKanasList+dakuonList+handakuonList+combosKanasList
+
+
+def randomRomajiToKana(alphabet, kanasSubsetIdx):
+    global KANASSUBSETS
+
+    global simpleKanasList
+    global dakuonList
+    global handakuonList
+    global combosKanasList
+
+    # The order must match with the input of selectKanasSubset
+    # KANASSUBSETS = ["simple", "Dakuon", "Handakuon", "combo"]
+
+    kanasSubsetString = KANASSUBSETS[kanasSubsetIdx-1]
+
+    if kanasSubsetIdx == 1:
+        usedKanasList = simpleKanasList
+    elif kanasSubsetIdx == 2:
+        usedKanasList = dakuonList
+    elif kanasSubsetIdx == 3:
+        usedKanasList = handakuonList
+    elif kanasSubsetIdx == 4:
+        usedKanasList = combosKanasList
 
     select1 = False
     score = 0
     while not select1:
-        print("# How many kanas for the game? (1-{})\n".format(KANASNUMBER))
+        print(
+            f"# How many {kanasSubsetString} for the game? (1-{len(usedKanasList)})\n")
         try:
-            choice = int(input("> "))
-            if choice >= 1 and choice <= KANASNUMBER:
+            nbKanas = int(input("> "))
+            if nbKanas >= 1 and nbKanas <= len(usedKanasList):
                 select1 = True
 
-                randKanasList = random.sample(kanasList, choice)
-                for game in range(0, choice):
-                    kana = randKanasList[game]
-                    input("\n#{}\n Draw {}".format(game+1, kana))
-                    image = Image.open("hiraganas/{}.png".format(kana))
-                    image.show()
+                randKanasList = random.sample(usedKanasList, nbKanas)
+                for idxKana in range(0, nbKanas):
+                    kana = randKanasList[idxKana]
+                    input(f"\n#{idxKana+1}\n Draw {kana}")
+                    image = Image.open("{}/{}.png".format(alphabet, kana))
+                    image.show(title="Kana #{}".format(idxKana))
                     image.close()
                     point = input("Press 1 if it is correct, 0 otherwise: ")
                     if point == '1':
                         score += 1
 
                 # Score
-                print("\n Score: {}/{}, {:.01f}%".format(score, choice,
-                                                    100.0*score/choice))
+                print(
+                    f"\n Score: {score}/{nbKanas}, {(100.0*score/nbKanas):.01f}%")
 
             else:
                 raise ValueError()
         except ValueError as err:
-            print("Wrong choice (must be 1-{})\n".format(KANASNUMBER))
+            print(f"Wrong choice (must be 1-{len(usedKanasList)})\n")
+        except FileNotFoundError as err:
+            print(f"WILL BE SOLVED SOON: missing image files: {err}\n")
+
+
+def selectKanasSubset(alphabet):
+    selected = False
+    while not selected:
+        print(
+            f"# Which {alphabet} do you want to write?\n 1- Simple {alphabet}\n 2- Dakuon\n 3- Handakuon\n 4- Combo {alphabet}\n 0- Return\n")
+        choice2 = input("> ")
+        selected = True
+
+        if choice2 == '1' or choice2 == '2' or choice2 == '3' or choice2 == '4':
+            randomRomajiToKana(alphabet, int(choice2))
+        elif choice2 == '0':
+            return 0
+        else:
+            selected = False
+            print("Wrong choice\n")
 
 
 def main():
-    global kanasList
-
     random.seed()
 
-    # for line in kanas:
-    #     for kana in kanas[line]:
-    #         image = Image.open("hiraganas/{}.png".format(kana))
-    #         image.show()
-    #         image.close
+    initListsFromKanasDicts()
+    selected = False
 
-    kanasList = []
-    for line in kanas:
-        for kana in kanas[line]:
-            kanasList.append(kana)
-
-    select1 = False
-    while not select1:
+    while not selected:
         print(
-            "# Select mode\n 1- Random Romaji->Hiragana\n 2- Random Romaji->Katakana\n")
-        choice = input("> ")
-        if choice == '1' or choice == '2':
-            select1 = True
-            randomRomajiToKana()
+            "# Select mode\n 1- Random Romaji->Hiragana\n 2- Random Romaji->Katakana\n 0- Exit\n")
+        choice1 = input("> ")
+        selected = True
+
+        if choice1 == '1':
+            selectKanasSubset(alphabet='Hiragana')
+        elif choice1 == '2':
+            selectKanasSubset(alphabet='Katakana')
+        elif choice1 == '0':
+            sys.exit(0)
         else:
+            selected = False
             print("Wrong choice\n")
 
 
